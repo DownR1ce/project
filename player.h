1c91397c725898e0322c77f37c78eddda87e10b4
DownR1ce <2818608850@qq.com>
2025-04-01 15:11:30 +0800

    Create player.h
    声明文件，将那几个变量变成全局变量。其他的文件只要include ”player.h“就可以使用这几个变量

---
+++ player.h
@@ -0,0 +1,16 @@
+//player.h
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+extern int player_heart; //玩家血量
+extern int number_of_mineSweeping; //玩家有限的扫雷次数
+extern vector <string> player_inventory; //特殊道具的储存空间
+extern int number_of_key; //玩家获取的钥匙数量
+
+
+#endif
