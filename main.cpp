dea968d0f931b67c5a18ea91ee8f6b3116245c90
DownR1ce <2818608850@qq.com>
2025-04-01 15:08:53 +0800

    Update main.cpp
    加了一些测试部分

--- main.cpp
+++ main.cpp
@@ -5,17 +5,22 @@ using namespace std;
 
 int main(){
     int x,y;
-    vector<vector<char>> map1;
-    cout<<"输入x和y构建迷宫(奇数 至少>7)"<<endl;
+    cout<<"输入x和y构建迷宫(奇数)"<<endl;
     cin >> x >> y;
     if (x%2==0||y%2==0){
         return 0;
     }
-    map1 = Make_Map(x, y);
+    Maps maps = Make_Map(x, y);
     //测试部分
-    for(int i = 0; i<map1.size(); i++){
+    for(int i = 0; i<maps.MazeMap_show.size(); i++){
         for(int j = 0; j < x; j++){
-            cout << map1[i][j];
+            cout << maps.MazeMap_show[i][j];
+        }
+        cout<<endl;
+    } 
+    for(int i = 0; i<maps.MazeMap_hide.size(); i++){
+        for(int j = 0; j < x; j++){
+            cout << maps.MazeMap_hide[i][j];
         }
         cout<<endl;
     } 
