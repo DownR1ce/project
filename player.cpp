4ecfefb46e9c7478681182e5891343ee48149455
DownR1ce <2818608850@qq.com>
2025-04-01 15:10:14 +0800

    Create player.cpp
    这个文档提供一些变量用来记录玩家的状态，现在有玩家血量，玩家物品栏，扫雷次数和找到的钥匙数量

---
+++ player.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int player_heart = 0;
+vector <string> player_inventory (1 , "nothing");
+int number_of_mineSweeping = 0;
+int number_of_key = 0;
