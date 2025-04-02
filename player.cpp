d5ca3d57f0a3fa3d0008b2d7d2cb5d64a7df4e77
DownR1ce <2818608850@qq.com>
2025-04-02 16:56:32 +0800

    Update player.cpp
    加了一个player坐标的全局变量

--- player.cpp
+++ player.cpp
@@ -3,7 +3,13 @@
 
 using namespace std;
 
+struct coordinate{
+    int y = 0;
+    int x = 0;
+};
+
 int player_heart = 0;
 vector <string> player_inventory (1 , "nothing");
 int number_of_mineSweeping = 0;
 int number_of_key = 0;
+coordinate Player_coordinate {0, 0};
