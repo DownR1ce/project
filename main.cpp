611346c5bcdf99b68a4b3dfc45a15c64ec273e91
DownR1ce <2818608850@qq.com>
2025-04-02 17:02:53 +0800

    Update main.cpp
    多include了几个文件，加了一个clean的指令，然后现在可以实现循环然后让玩家移动，碰到地雷或者墙壁或者神秘小门都会输出相对应的提示。具体功能还没做，有待完善。现在文件数量太多了，急需一个MakeFile，谁能搞一下？如果有人看到的话 O.o

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "MazeMap.h"
+#include "player.h"
+#include "movement.h"
+#include <string>
 #include <iostream>
 
 using namespace std;
@@ -7,22 +10,41 @@ int main(){
     int x,y;
     cout<<"输入x和y构建迷宫(奇数)"<<endl;
     cin >> x >> y;
+    cout << "\033[2J\033[H";
     if (x%2==0||y%2==0){
         return 0;
     }
     Maps maps = Make_Map(x, y);
     //测试部分
-    for(int i = 0; i<maps.MazeMap_show.size(); i++){
-        for(int j = 0; j < x; j++){
-            cout << maps.MazeMap_show[i][j];
-        }
-        cout<<endl;
-    } 
     for(int i = 0; i<maps.MazeMap_hide.size(); i++){
         for(int j = 0; j < x; j++){
             cout << maps.MazeMap_hide[i][j];
         }
         cout<<endl;
     } 
+    for(int i = 0; i<maps.MazeMap_show.size(); i++){
+        for(int j = 0; j < x; j++){
+            cout << maps.MazeMap_show[i][j];
+        }
+        cout<<endl;
+    } 
+    while(true){
+        string order;
+        cout << "please enter 'wasd' to control player" << endl;
+        cin >> order;
+        cout << "\033[2J\033[H";
+        if (order == "e"){
+            break;
+        }
+        if (order == "w" || order == "s" || order == "a" || order == "d"){
+            player_movement(order, maps);
+        }
+        for(int i = 0; i<maps.MazeMap_show.size(); i++){
+            for(int j = 0; j < x; j++){
+                cout << maps.MazeMap_show[i][j];
+            }
+            cout<<endl;
+        }
+    }
     return 0;
 }
