cb1ded566367ed3a1a43d36dfb21589dd07df9a5
DownR1ce <2818608850@qq.com>
2025-04-02 17:00:11 +0800

    Update Element.h
    也是，把player.h加进来了

--- Element.h
+++ Element.h
@@ -4,16 +4,12 @@
 
 #include <random>
 #include <vector>
+#include "player.h"
 
 using namespace std;
 
 extern random_device rd;
 extern mt19937 gen;
-
-struct coordinate{
-    int y = 0;
-    int x = 0;
-};
 
 void add_door_for_small_game (vector <vector <char>>& map1, vector <coordinate> Sihutong, int x, int y);
 
