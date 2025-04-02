b0a5b788e8e0b81a7b7fd4d2c99fdf1bc8a23346
DownR1ce <2818608850@qq.com>
2025-04-02 16:55:35 +0800

    Create movement.cpp
    这个是一个用来写所有关于移动方法的文件，现在包含player的移动方法。包括player移动时对地图做出的一些改变

---
+++ movement.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <iostream>
+#include "player.h"
+#include "MazeMap.h"
+
+void player_movement (string order, Maps& maps){
+    coordinate maybe;
+    if (order == "w"){
+        maybe = {Player_coordinate.y - 1 , Player_coordinate.x};
+    }
+    if (order == "s") {
+        maybe = {Player_coordinate.y + 1 , Player_coordinate.x};
+    }
+    if (order == "a"){
+        maybe = {Player_coordinate.y, Player_coordinate.x - 1};
+    }
+    if (order == "d"){
+        maybe = {Player_coordinate.y, Player_coordinate.x + 1};
+    }
+    if (maps.MazeMap_show[maybe.y][maybe.x] != '#' || maps.MazeMap_show[maybe.y][maybe.x] != '@'){
+        if (maps.MazeMap_hide[maybe.y][maybe.x] == '.' || maps.MazeMap_hide[maybe.y][maybe.x] == 'S'){
+            maps.MazeMap_show [Player_coordinate.y][Player_coordinate.x] = '.';
+            Player_coordinate = maybe;
+            maps.MazeMap_show[maybe.y][maybe.x] = 'P';
+        }
+        if (maps.MazeMap_hide[maybe.y][maybe.x] == 'X'){
+            cout<< "你踩到了炸弹" << endl;
+            maps.MazeMap_show [Player_coordinate.y][Player_coordinate.x] = '.';
+            Player_coordinate = maybe;
+            maps.MazeMap_show[maybe.y][maybe.x] = 'X';
+        }
+        if (maps.MazeMap_hide[maybe.y][maybe.x] == '?'){
+            cout << "你找到了神秘的门, 按e进入" << endl;
+            maps.MazeMap_show[maybe.y][maybe.x] = '?';
+        }
+        if (maps.MazeMap_hide[maybe.y][maybe.x] == 'D'){
+            cout << "你找到了出口，但是钥匙呢？\n 摁e开锁" << endl;
+            maps.MazeMap_show[maybe.y][maybe.x] = 'D';
+        }
+    }
+    else{
+        cout << "不能撞墙" << endl;
+    }
+    return;
+}
