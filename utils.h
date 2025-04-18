9b766d8e43b5b7ec4a385c5bb7685da08602559d
DownR1ce <2818608850@qq.com>
2025-04-19 01:13:56 +0800

    Create utils.h
    声明文件

---
+++ utils.h
@@ -0,0 +1,14 @@
+//utils.h
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+pair<int, int> getTerminalSize();
+void printCentered(const vector<string>& lines);
+
+#endif
