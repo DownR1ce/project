2df51d8d4633c1ca6e9ba3004883b7461a40380f
DownR1ce <2818608850@qq.com>
2025-04-19 01:46:49 +0800

    Update player.cpp
    获取血量的方法我加到这个player.cpp里面了

--- player.cpp
+++ player.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -9,6 +10,13 @@ struct coordinate{
 };
 
 int player_heart = 0;
+
+void getHeart(){
+    cout << "请输入玩家血量（最后这个会改成让玩家选难度，然后每个难度不同血量）" << endl;
+    cin >> player_heart;
+    return;
+}
+
 vector <string> player_inventory (1 , "nothing");
 int number_of_mineSweeping = 0;
 int number_of_key = 0;
