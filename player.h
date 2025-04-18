b129b65e9bd1e45a5d30bc0a18d5790a39821217
DownR1ce <2818608850@qq.com>
2025-04-19 01:47:11 +0800

    Update player.h

--- player.h
+++ player.h
@@ -17,6 +17,7 @@ extern int number_of_mineSweeping; //玩家有限的扫雷次数
 extern vector <string> player_inventory; //特殊道具的储存空间
 extern int number_of_key; //玩家获取的钥匙数量
 extern coordinate Player_coordinate; //玩家的坐标
+void getHeart();
 
 
 #endif
