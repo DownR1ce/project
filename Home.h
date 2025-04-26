a62eb6f232c26dcff987bca032b7774db084b311
DownR1ce <2818608850@qq.com>
2025-04-26 23:38:53 +0800

    Create Home.h

---
+++ Home.h
@@ -0,0 +1,21 @@
+//Home.h
+#ifndef HOME_H
+#define HOME_H
+
+#include <vector>
+#include "player.h"
+
+using namespace std;
+
+void DisplayTitle();
+void GameChoice();
+void HomePage();
+void setColor(int color);
+void initConsole();
+string random_tips();
+void showLoadingBar();
+void showLoadingScreen();
+vector <int> getXY();
+
+
+#endif
