4e353901f03b9de778c0a2051f33ca39e84afd13
DownR1ce <2818608850@qq.com>
2025-04-29 21:31:34 +0800

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+vector <string> NameSaved;
+
 struct coordinate{
     int y = 0;
     int x = 0;
@@ -23,9 +25,22 @@ void getHeart(){
     return;
 }
 
+void PrintSaved(){
+    if(NameSaved.size()>0){
+        for(int i = 0; i<NameSaved.size(); i++){
+            cout << NameSaved[i] << endl;
+        }
+        cout<<"\n\n\nchoose one";
+    }
+    if(NameSaved.size()==0){
+        cout << "There do not have your game, go to start a new game =!!";
+    }
+}
+
 vector <string> player_inventory (1 , "nothing");
 int number_of_mineSweeping = 0;
 int number_of_key = 0;
 int XuYaoDe_number_of_key = 0;
 coordinate Player_coordinate {0, 0};
 int number_of_mine = 0;
+
